67778b7d5f9b371631981ced6f969393d64b8c16
Jon LaBelle <contact@jonlabelle.com>
2024-11-08 10:08:09 -0600

    refactor: generic container verbiage

--- src/app.mjs
+++ src/app.mjs
@@ -6,10 +6,22 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 const isDocker = process.env.RUNNING_IN_DOCKER === 'true';
 
+const baseUrl = `http://localhost:${port}`;
+
 routes(app);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log('-'.repeat(40));
+  console.log(' '.repeat((40 - 'Serve JSON'.length) / 2) + 'Serve JSON');
+  console.log(`${'-'.repeat(40)}`);
+  console.log('');
+  console.log('A simple JSON server for your local development.');
+  console.log('');
+  console.log(`- Server is running on....: ${baseUrl}`);
+  console.log(`- JSON files URI..........: ${baseUrl}/data/<your-data>.json`);
+  console.log(`- Sample JSON file........: ${baseUrl}/data/sample.json`);
+  console.log(`- Browse JSON files.......: ${baseUrl}/list-data`);
+  console.log('');
 
   if (!isDocker) {
     console.log(
@@ -17,6 +29,6 @@ app.listen(port, () => {
     );
     open(`http://localhost:${port}/list-data`);
   } else {
-    console.log('Running in Docker container - browser auto-open disabled');
+    console.log('Running in a container... browser auto-open disabled.');
   }
 });
